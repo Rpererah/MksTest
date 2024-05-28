bb6be1b9dfedc4e6246b5c0bb631d5aca567365a
rpererah <rpererah@gmail.com>
2024-05-27 23:17:51 -0300

    :art: structures and configuring environment

---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    
+*{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+  }
+
+  body{
+    background-color: ${(props) => props.theme.white};
+    color: ${(props) => props.theme['gray-600']};
+    -webkit-font-smoothing:antialiased
+}
+
+:focus{
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme['gray-600']};
+}
+
+body,input,button{
+    font-style: 'Montserrat';
+    font-weight: 400;
+    font-size: 1rem;
+
+}
+
+`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,11 @@
+export const defaultTheme = {
+  'gray-100': '#F9F9F9',
+  'gray-200': '#EEEEEE',
+  'gray-400': '#BFBFBF',
+  'gray-600': '#2C2C2C',
+  'gray-900': '#373737',
+  blue: '#0F52BA',
+  yellow: '#FFFF00',
+  black: '#000000',
+  white: '#FFFFFF',
+}
